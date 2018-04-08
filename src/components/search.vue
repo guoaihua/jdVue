4708ff2c2a61298d45b9c3981a92acf12e13add9
guoaihua <guoaihua@geetest.com>
2018-04-08 18:47:28 +0800

    update

---
+++ src/components/search.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="search">
+    <el-input
+      placeholder="请输入内容"
+      prefix-icon="el-icon-search"
+      v-model="inputData">
+    </el-input>
+    <icon class="phone-icon" name="phone"></icon>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'search',
+  data () {
+    return {
+      inputData: ''
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  #search {
+    height: 50px;
+    display: flex;
+    background-color: #ffffff6e;
+    justify-content: center;
+    align-items: center;
+    .el-input {
+      width: 80%;
+     input .el-input__inner {
+        border-radius: 25px;
+      }
+    }
+    .phone-icon {
+      width: 40px;
+      height: 40px;
+    }
+  }
+</style>
--- src/pages/app-main.vue
+++ src/pages/app-main.vue
@@ -1,7 +1,16 @@
 <template>
   <div id="appMain">
     <el-container>
-      <el-main>Main</el-main>
+      <el-main>
+        <search></search>
+        <template>
+          <el-carousel indicator-position="outside">
+            <el-carousel-item v-for="img in imgs" :key="img.id">
+              <h3><img  :src="img" ></h3>
+            </el-carousel-item>
+          </el-carousel>
+        </template>
+      </el-main>
       <el-footer>
         <my-footer></my-footer>
       </el-footer>
@@ -11,16 +20,29 @@
 
 <script>
 import myFooter from '../components/myFooter'
+import search from '../components/search'
+import img1 from '../assets/imgs/bg1.jpg'
 export default {
   name: 'appMain',
+  data () {
+    return {
+      imgs: [{
+        img1,
+        id: 0
+      }, {
+        img1,
+        id: 1
+      }]
+    }
+  },
   components: {
-    myFooter
+    myFooter, search
   }
 }
 </script>
 
 <style scoped lang="scss">
-  div {
+  #appMain {
     width: 100%;
     height: 100%;
     .el-container {
@@ -28,7 +50,21 @@ export default {
       width: 100%;
       height: 100%;
       .el-main {
+        padding: 0;
         background-color: gold;
+        #search {
+          position: fixed;
+          top: 0;
+          width: 100%;
+          z-index:999;
+        }
+        .el-carousel__item h3 {
+          color: #475669;
+          font-size: 18px;
+          opacity: 0.75;
+          line-height: 183px;
+          margin: 0;
+        }
       }
       .el-footer {
         padding: 0;
